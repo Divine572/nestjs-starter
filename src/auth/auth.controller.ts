1c04a9d1fac94fa4804344a39a55f28fa1d64611
divicodes <divinef837@gmail.com>
2022-09-05 23:47:34 +0100

    added localAuthGuard and extended Request interface

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegistrationDto } from './dto/registration.dto';
+import { LocalAuthGuard } from './localAuth.guard';
+import RequestWithUser from './requestWithUser.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registrationData: RegistrationDto) {
+    return this.authService.register(registrationData);
+  }
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req: RequestWithUser) {
+    const user = req.user;
+    user.password = undefined;
+    return user;
+  }
+}
